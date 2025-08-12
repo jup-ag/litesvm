49146c2bd79237a02c93e69622b7b4bd276aaf80
Arrowana <Arrowana@users.noreply.github.com>
2025-08-12 17:15:13 +1000

    Update token 2022 to latest

--- crates/litesvm/src/spl/mod.rs
+++ crates/litesvm/src/spl/mod.rs
@@ -7,7 +7,7 @@ pub fn load_spl_programs(svm: &mut LiteSVM) {
     );
     svm.add_program(
         pubkey!("TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb"),
-        include_bytes!("programs/spl_token_2022-5.0.2.so"),
+        include_bytes!("programs/spl_token_2022-mainnet.so"),
     );
     svm.add_program(
         pubkey!("Memo1UhkJRfHyvLMcVucJwxXeuD728EqVDDwQDxFMNo"),
